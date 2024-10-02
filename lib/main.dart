6c8ce58551a8114a7387493cadce0d93434d4bfc
Anton M. <antonmenshickov@gmail.com>
2024-10-02 19:31:26 +0700

    problem with _mapObjectManager.removeObject

--- lib/main.dart
+++ lib/main.dart
@@ -49,31 +49,79 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late final sdk.Context _mapContext;
   final _mapWidgetController = sdk.MapWidgetController();
+  late final sdk.MapObjectManager _mapObjectManager;
+  late final sdk.ImageLoader _loader;
+  late final sdk.Image _pointImage;
+  late final List<sdk.Marker> _markers;
 
   @override
   void initState() {
+    _markers = [];
     _mapContext = sdk.DGis.initialize(
       logOptions: const sdk.LogOptions(
-        customLogLevel: sdk.LogLevel.verbose,
-        logLevel: sdk.LogLevel.verbose,
+        customLogLevel: sdk.LogLevel.error,
+        logLevel: sdk.LogLevel.error,
       ),
       httpOptions: const sdk.HttpOptions(
         timeout: Duration(seconds: 5),
       ),
     );
+    _loader = sdk.ImageLoader(_mapContext);
+    _mapWidgetController.getMapAsync(_onMapReady);
     super.initState();
+  }
+
+  _onMapReady(sdk.Map map) async {
+    _mapObjectManager = sdk.MapObjectManager(map);
+    _pointImage =
+        await _loader.loadPngFromAsset('assets/point_grey.png', 100, 100);
+    _testPoint();
+  }
+
+  _testPoint() async {
+    _markers.clear();
+    print('adding 100 markers to map');
+    for (int i = 0; i < 100; i++) {
+      _markers.add(sdk.Marker(
+        sdk.MarkerOptions(
+            icon: _pointImage,
+            position: sdk.GeoPointWithElevation(
+              latitude: sdk.Latitude(58.2855 + i * 0.01),
+              longitude: sdk.Longitude(104.2890 + i * 0.01),
+            ),
+            userData: i.toString(),
+            zIndex: sdk.ZIndex(i),
+            iconWidth: const sdk.LogicalPixel(1.0)),
+      ));
+      _mapObjectManager.addObject(_markers[i]);
+    }
+    print('waiting 5 seconds...');
+
+    await Future.delayed(const Duration(seconds: 5));
+    print('removing add markers');
+    for (var marker in _markers) {
+      _mapObjectManager.removeObject(marker);
+    }
+    _markers.clear();
+    print('done.');
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map'),
       ),
       body: sdk.MapWidget(
         sdkContext: _mapContext,
-        mapOptions: sdk.MapOptions(),
+        mapOptions: sdk.MapOptions(
+          position: const sdk.CameraPosition(
+              point: sdk.GeoPoint(
+                latitude: sdk.Latitude(58.2855),
+                longitude: sdk.Longitude(104.2890),
+              ),
+              zoom: sdk.Zoom(15.0)),
+        ),
         controller: _mapWidgetController,
       ),
     );
